61639ef435b45813451f43009e3fe86e784a7512
Bjarke Wedemeijer <bjarke@wedemeijer>
2018-04-22 09:21:54 +0200

    added some new files  :D

---
+++ gitaliases.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+alias gb="git branch"
+alias gba="git branch -a"
+alias gc="git commit -v"
+alias gd="git diff | mate"
+alias gl="git pull"
+alias gp="git push"
+alias gst="git status"
+
+
+
